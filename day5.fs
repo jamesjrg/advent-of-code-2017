5c9d4623f17ab494ecb3dc3cd9292d2332bf1140
James Gregory <james.jrg@gmail.com>
2017-12-09 23:55:05 +0000

    day 5 part two

--- day5.fs
+++ day5.fs
@@ -6,7 +6,7 @@ let test1 = "0
 1
 -3"
 
-let day5Calculator (text:string) = 
+let partOne (text:string) = 
     let jumpList = text.Split('\n') |> Array.map int
     
     let rec jump pointer count =
@@ -18,10 +18,27 @@ let day5Calculator (text:string) =
             jump (pointer + oldJump) (count + 1)
 
     jump 0 0
+
+let partTwo (text:string) = 
+    let jumpList = text.Split('\n') |> Array.map int
     
+    let rec jump pointer count =
+        if pointer < 0 || pointer >= jumpList.Length then
+            count
+        else
+            let oldJump = jumpList.[pointer]
+            
+            let jumpIncDec = if oldJump >= 3 then -1 else 1
+            
+            jumpList.[pointer] <- oldJump + jumpIncDec
+
+            jump (pointer + oldJump) (count + 1)
+
+    jump 0 0    
 
 let day5 () =
     let puzzleInput = System.IO.File.ReadAllText("day5input.txt");
     
-    printfn "%d" (day5Calculator test1)
-    printfn "%d" (day5Calculator puzzleInput)
+    printfn "%d" (partOne test1)
+    printfn "%d" (partOne puzzleInput)
+    printfn "%d" (partTwo puzzleInput)
