329cafbe0e549903abd7dbd9290b986763d1797a
James Gregory <james.jrg@gmail.com>
2017-12-09 21:34:32 +0000

    day 4 part two

--- day4.fs
+++ day4.fs
@@ -1,22 +1,43 @@
 module Advent.Day4
 
-let test1 = "aa bb cc dd ee" // is valid
-let test2 = "aa bb cc dd aa" // is not valid - the word aa appears more than once.
-let test3 = "aa bb cc dd aaa" // is valid
+let partOneTests = [
+    ("aa bb cc dd ee", true)
+    ("aa bb cc dd aa" , false)
+    ("aa bb cc dd aaa", true)
+]
 
-let checkValid (passphrase:string) = 
+let partTwoTests = [
+    ("abcde fghij", true)
+    ("abcde xyz ecdab" , false)
+    ("a ab abc abd abf abj", true)
+    ("iiii oiii ooii oooi oooo", true)
+    ("oiii ioii iioi iiio", false)
+]
+
+let checkValidPartOne (passphrase:string) = 
     let words = passphrase.Split()
     words |> Seq.groupBy id |> Seq.where (fun (k, v) -> Seq.length v > 1) |> Seq.isEmpty
 
-let day4Calculator (text:string) = 
+let checkValidPartTwo (passphrase:string) = 
+    passphrase.Split()
+        |> Seq.map ((Seq.sort) >> (fun x -> System.String.Concat(x)))
+        |> Seq.groupBy id
+        |> Seq.where (fun (k, v) -> Seq.length v > 1)
+        |> Seq.isEmpty
+
+
+let countValidPartOne (text:string) = 
     let rows = text.Split('\n')
-    rows |> Seq.where checkValid |> Seq.length
-    
+    rows |> Seq.where checkValidPartOne |> Seq.length    
+
+let countValidPartTwo (text:string) = 
+    let rows = text.Split('\n')
+    rows |> Seq.where checkValidPartTwo |> Seq.length    
 
 let day4 () =
     let puzzleInput = System.IO.File.ReadAllText("day4input.txt");
-    
-    printfn "%b" (checkValid test1)
-    printfn "%b" (checkValid test2)
-    printfn "%b" (checkValid test3)
-    printfn "%d" (day4Calculator puzzleInput)
+
+    for input, expected in partTwoTests do
+        printfn "expected: %b actual: %b" expected (checkValidPartTwo input)
+
+    printfn "%d" (countValidPartTwo puzzleInput)    
